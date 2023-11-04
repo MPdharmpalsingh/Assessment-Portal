724295e623fbd9cc71e1fda9d71ab4c1b70f44dd
dharmpal <dps33261@gmail.com>
2023-11-04 12:39:06 +0530

    table are mapped in DB

---
+++ examserver/src/main/java/com/exam/model/Role.java
@@ -0,0 +1,105 @@
+package com.exam.model;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="roles")
+public class Role {
+    @Id
+    private Long roleId;
+    private String roleName;
+    @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.LAZY,mappedBy = "role")
+   private Set<UserRole>userRoles=new HashSet<>();
+
+    public Role(Long roleId, String roleName) {
+        this.roleId = roleId;
+        this.roleName = roleName;
+    }
+
+    public Set<UserRole> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(Set<UserRole> userRoles) {
+        this.userRoles = userRoles;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//package com.exam.model;
+//
+//import jakarta.persistence.*;
+//
+//import java.util.HashSet;
+//import java.util.Set;
+//
+//@Entity
+//@Table(name="roles")
+//public class Role {
+//    @Id
+//    private  long roleid;
+//    private  String roleName;
+//    @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.LAZY,mappedBy = "role")
+//    private Set<UserRole>userRoles=new HashSet<>();
+//
+//    public Role() {
+//    }
+//
+//    public Set<UserRole> getUserRoles() {
+//        return userRoles;
+//    }
+//
+//    public void setUserRoles(Set<UserRole> userRoles) {
+//        this.userRoles = userRoles;
+//    }
+//
+//    public long getRoleid() {
+//        return roleid;
+//    }
+//
+//    public Role(long roleid, String roleName) {
+//        this.roleid = roleid;
+//        this.roleName = roleName;
+//    }
+//
+//    public void setRoleid(long roleid) {
+//        this.roleid = roleid;
+//    }
+//
+//    public String getRoleName() {
+//        return roleName;
+//    }
+//
+//    public void setRoleName(String roleName) {
+//        this.roleName = roleName;
+//    }
+//}
